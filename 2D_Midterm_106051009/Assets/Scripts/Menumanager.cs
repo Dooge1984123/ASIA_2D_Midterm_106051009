88a9ea71a7e822f81561d3a9bd98bb3905f3ff86
Dooge1984123 <71687804+Dooge1984123@users.noreply.github.com>
2020-11-17 04:24:51 +0800

    1117

---
+++ 2D_Midterm_106051009/Assets/Scripts/Menumanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menumanager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡");
+        //SceneManager.LoadScene(1);
+    }
+
+ 
+    public void QuitGame()
+    {
+        print("結束遊戲");
+        Application.Quit();
+    }
+}
---
+++ 2D_Midterm_106051009/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(true)
+        print("碰到傳送們");
+        final.SetActive(true);
+    }
+
+}
